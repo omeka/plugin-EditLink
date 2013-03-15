122b3052a86726bc7db9fd7752990bee2bbaed42
patrickmj <patrickmjchnm@gmail.com>
2013-03-15 10:13:48 -0400

    First commit

---
+++ EditLinkPlugin.php
@@ -0,0 +1,46 @@
+<?php
+
+class EditLinkPlugin extends Omeka_Plugin_AbstractPlugin
+{
+    protected $_filters = array('public_navigation_admin_bar');
+
+    public function filterPublicNavigationAdminBar($navLinks)
+    {
+        $view = get_view();
+        if(isset($view->item)) {
+            $record = $view->item;
+            $aclRecord = $view->item;
+        }
+        
+        if(isset($view->collection)) {
+            $record = $view->collection;
+            $aclRecord = $view->collection;
+        }
+        
+        if(isset($view->exhibit_page)) {
+            $record = $view->exhibit_page;
+            $aclRecord = $view->exhibit;
+        }                
+        
+        if(!isset($record)) {
+            return $navLinks;
+        }
+        
+        if(is_allowed($aclRecord, 'edit')) {
+            set_theme_base_url('admin');
+            if(get_class($record) == 'ExhibitPage') {
+                $url = url('exhibits/edit-page-content/' . $record->id);
+            } else {
+                $url = record_url($record, 'edit');
+            }
+            //want to place it first in the navigation, so do an array merge
+            $editLinks['Edit Link'] = array(
+                    'label'=>'Edit',
+                    'uri'=> $url
+                    );
+            revert_theme_base_url();
+        }
+        $navLinks = array_merge($editLinks, $navLinks);
+        return $navLinks;
+    }
+}
